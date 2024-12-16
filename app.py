ca66a189e5018d8a5dabfa621fa4a8dc51fc692f
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2024-12-16 09:23:17 +0100

    check

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
+import random
 from country_list import get_countries
 
 countries = get_countries()
-print(countries)
+# print(countries)
+
+generalt_szo = random.choice(countries)
+
+print(generalt_szo)
+
+mondott_betu = input("Adj meg egy betűt! ")
+
+if mondott_betu in generalt_szo:
+    print("yes")
+else:
+    print("nah")
