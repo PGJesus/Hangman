b301256060fa1b2b32aed2ce643eb41fd156924b
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2024-12-16 09:41:37 +0100

    alulvonasok_half

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ countries = get_countries()
 # print(countries)
 
 generalt_szo = random.choice(countries)
-
+nemtalalatok = 0
 print(generalt_szo)
+
+betu_szam = len(generalt_szo)
+for betu in generalt_szo:
+    print("_ ")
+
+    if " " in generalt_szo:
+        print(" ")
+
 
 mondott_betu = input("Adj meg egy betűt! ")
 
@@ -14,3 +22,6 @@ if mondott_betu in generalt_szo:
     print("yes")
 else:
     print("nah")
+    nemtalalatok += 1
+
+print(nemtalalatok)
